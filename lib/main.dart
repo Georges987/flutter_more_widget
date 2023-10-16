ce732939554b3035dce817f7ab2e45bbbe3d46e3
= <=>
2023-10-16 12:29:01 +0100

    Adding structure

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  Widget spacer = const SizedBox(height: 18);
+
   int _selectedIndex = 0;
   final _nameConttroller = TextEditingController();
   String? _sexe;
@@ -45,50 +47,106 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            DrawerHeader(
-              decoration: const BoxDecoration(color: Colors.red),
-              child: Text(
-                _name,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              DrawerHeader(
+                decoration: const BoxDecoration(color: Colors.red),
+                child: Text(
+                  _name,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
                 ),
               ),
-            ),
-            const ListTile(
-              leading: Icon(Icons.message),
-              title: Text("Messages"),
-            ),
-            const ListTile(
-              leading: Icon(Icons.person),
-              title: Text("Profil"),
-            ),
-            const ListTile(
-              leading: Icon(Icons.settings),
-              title: Text("Setting"),
-            ),
+              const ListTile(
+                leading: Icon(Icons.message),
+                title: Text("Messages"),
+              ),
+              const ListTile(
+                leading: Icon(Icons.person),
+                title: Text("Profil"),
+              ),
+              const ListTile(
+                leading: Icon(Icons.settings),
+                title: Text("Setting"),
+              ),
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          title: const Text("TP 2"),
+          backgroundColor: Colors.purple,
+          actions: [
+            IconButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text("Mes notifications")));
+                },
+                icon: const Icon(Icons.add_alert)),
+            IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
           ],
         ),
-      ),
-      appBar: AppBar(
-        title: const Text("TP 2"),
-        backgroundColor: Colors.purple,
-        actions: [
-          IconButton(
-              onPressed: () {
-                ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text("Mes notifications")));
-              },
-              icon: const Icon(Icons.add_alert)),
-          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
-        ],
-      ),
-      body: const Center(
-        child: Text("Hello"),
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const Text(
+                  "Information de l'utilisateur",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 18),
+                Image.asset(
+                  'assets/image/logo.png',
+                  width: 125,
+                ),
+                const SizedBox(height: 20),
+                Form(child: Column()),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    _display ? const Card() : const SizedBox(),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: "Inscription",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.info),
+              label: "Information",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: "Paramètres",
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          onTap: (int index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text("Float action button pressed"))),
+          tooltip: 'Increment',
+          shape: const CircleBorder(),
+          backgroundColor: Colors.purple,
+          child: const Icon(Icons.add),
+        ));
   }
 }
